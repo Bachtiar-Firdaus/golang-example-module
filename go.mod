0a84b4af6600b4838e4da3aa43f0191be6deace1
bachtiar firdaus <bachtiar.firdaus@dansmultipro.com>
2022-11-06 14:18:17 +0700

    Belajar penggunaan go-modules Major Upgrade module

--- go.mod
+++ go.mod
@@ -4,5 +4,10 @@ go 1.19
 
 // versi sebelum
 // require github.com/Bachtiar-Firdaus/belajar-golang-modules v1.0.0 // indirect
-// upgrade depedency
-require github.com/Bachtiar-Firdaus/belajar-golang-modules v1.5.1
+// upgrade depedency dan kemudian lakukan comand go get
+// untuk mengganti v1 satu ke versi dua tinggal di coman versi yang lama
+// require github.com/Bachtiar-Firdaus/belajar-golang-modules v1.5.1 // indirect
+// kemudina lakukan comand
+// go get github.com/Bachtiar-Firdaus/belajar-golang-modules/v2
+
+require github.com/Bachtiar-Firdaus/belajar-golang-modules/v2 v2.0.0 // indirect
--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	// disini kita menambahkan module yang telah kita buat tadi
-	go_say_hello "github.com/Bachtiar-Firdaus/belajar-golang-modules"
+	// go_say_hello "github.com/Bachtiar-Firdaus/belajar-golang-modules" kita comand module yang lama
+	// disini kita gunakan module yang v2
+	go_say_hello "github.com/Bachtiar-Firdaus/belajar-golang-modules/v2"
 )
 
 func main() {
 	// setelah kita menambahkan module kita bisa langsung menggunakan module yang telah kita buat
 	fmt.Println(go_say_hello.SayHello())
 	// kemudian kita bisa langsung menggunakan depedency injection yang baru
-	fmt.Println(go_say_hello.SayName())
+	fmt.Println(go_say_hello.SayName("daus"))
 }
