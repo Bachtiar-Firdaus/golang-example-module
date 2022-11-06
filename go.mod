9ecc7b4c768e944264b4b10f970aa1d9f855f428
bachtiar firdaus <bachtiar.firdaus@dansmultipro.com>
2022-11-06 13:35:28 +0700

    Belajar penggunaan go-modules

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Bachtiar-Firdaus/golang-example-module
+
+go 1.19
+
+require github.com/Bachtiar-Firdaus/belajar-golang-modules v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	// disini kita menambahkan module yang telah kita buat tadi
+	go_say_hello "github.com/Bachtiar-Firdaus/belajar-golang-modules"
+)
+
+func main() {
+	// setelah kita menambahkan module kita bisa langsung menggunakan module yang telah kita buat
+	fmt.Println(go_say_hello.SayHello())
+}
